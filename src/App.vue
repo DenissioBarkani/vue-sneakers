0bf707be76715df8b618575cee6ff99adbe46d16
Denissio <my.email@example.com>
2025-09-29 23:17:38 +0300

    vue-sneakers-start

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import MyHello from './components/MyHello.vue'
+</script>
+
+<template>
+  <MyHello title="Кроссовки Пума" :price="9500" :isAdded="true"> <button>123</button></MyHello>
+
+  <MyHello />
+
+  <MyHello />
+</template>
+
+<style scoped></style>
---
+++ src/components/Learn.vue
@@ -0,0 +1,61 @@
+ <!-- опшенс апи -->
+<!-- <script>
+export default {
+    data() {
+        return {
+            count: 0
+        }
+    },
+    methods: {
+        increment() {
+            this.count++
+        },
+        decriment() {
+            this.count--
+        }
+    }
+}
+</script> -->
+<!-- <template>
+  <p>{{ count }}</p>
+  <button @click="increment">+</button>
+</template>
+ -->
+
+ <!-- копозишон апи -->
+<!-- <script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+function increment() {
+    count.value++
+  console.log(count)
+}
+</script>
+
+<template>
+  <p>{{ count }}</p>
+  <button @click="increment">+</button>
+</template> -->
+
+
+
+<!-- <script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+function increment() {
+    count.value++
+  console.log(count)
+}
+
+// function updateCount(event) {
+//     count.value = Number(event.target.value)
+// }
+</script>
+
+<template>
+  <p>{{ count }}</p>
+  <button @click="increment">+</button>
+  <input v-model="count"/>
+</template> -->
